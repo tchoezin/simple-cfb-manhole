1f044bcfc995702f086445b2b5fe3bb53ba5db54
tchoezin <tchoezin17@gmail.com>
2026-08-28 14:04:27 -0500

    adding deploy to github pages workflow, updating vite.config.ts file to build under correct subpath

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/simple-cfb-manhole/",
   plugins: [react()],
   test: {
     globals: true,
